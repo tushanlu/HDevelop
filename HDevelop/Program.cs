ed93475c0e85f7d2c54c070eb078b128ac949280
tushanlu <483373342@qq.com>
2022-12-14 17:41:15 +0800

    备份

---
+++ HDevelop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace HDevelop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sPath = Environment.GetEnvironmentVariable("HALCONROOT");
+            string sPlatform = Environment.GetEnvironmentVariable("HALCONARCH");
+
+            if (sPath == null || sPlatform == null)
+            {
+                Console.WriteLine(">> 未找到Halcon程序");
+                Console.ReadKey();
+                return;
+            }
+
+            System.Diagnostics.Process exep = new System.Diagnostics.Process();
+            exep.StartInfo.FileName = sPath + "\\bin\\" + sPlatform + "\\hdevelop.exe";
+            exep.StartInfo.Arguments = "";
+            exep.StartInfo.CreateNoWindow = false;
+            exep.StartInfo.UseShellExecute = false;
+            exep.Start();
+            
+        }
+    }
+}
